fbfc95e25ae64de84215bb0a74c987f83a15052a
Elif Soy <esoy139@gmail.com>
2026-03-29 06:44:49 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -315,6 +315,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(PORT, "127.0.0.1", () => {
-  console.log(`BEWARE!: JOBS server calisiyor: http://127.0.0.1:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`BEWARE!: JOBS server calisiyor: http://0.0.0.0:${PORT}`);
 });
